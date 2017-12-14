ade7738fdca9e0714fb97d1f493c0f60922a23c8
Syed Shah <syed.shah@nyu.edu>
2017-12-14 12:37:21 -0500

    Create posts_controller

---
+++ MemeApp/app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApiController
+  before_action :require_login, except: [:index, :show]
+
+  def index
+    posts = Post.all
+    render json: { posts: posts }
+  end
+
+  def show
+    post = Post.find(params[:id])
+    post_user = post.user
+    render json: { post: post, username: post_user.username }
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user = current_user
+
+    if post.save
+      render json: {
+        message: 'ok',
+        monster: post,
+      }
+    else
+      render json: {message: 'Could not create post'}
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :url)
+  end
+end
