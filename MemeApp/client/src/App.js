4d27af9fca51e58ea476dcfbad8e49891f18218f
Syed Shah <syed.shah@nyu.edu>
2017-12-14 16:48:45 -0500

    Create a route for '/posts', Create a PostList component, Update App.js to render a list of posts (our PostList.jsx)

--- MemeApp/client/src/App.js
+++ MemeApp/client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
+import Auth from './modules/Auth'
+import PostList from './components/PostList';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      auth: Auth.isUserAuthenticated(),
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/posts" render={() => <PostList />} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ MemeApp/client/src/components/PostList.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postList: null,
+      postListLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('/posts')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({
+          postList: res.posts,
+          postListLoaded: true
+        })
+      }).catch(err => console.log(err));
+  }
+
+  renderPosts() {
+    return this.state.postList.map(post => {
+      return (
+        <div className="post" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
+          <img src={`${post.url}`} width={500} height={400} mode='fit' />
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="post-list">
+        {this.state.postListLoaded ?
+          this.renderPosts() : <p>Loading...</p>
+        }
+      </div>
+    )
+  }
+}
+
+export default PostList;
